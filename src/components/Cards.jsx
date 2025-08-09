32f573a0e64d82279b3cef7743ea9f95ce133f60
crimso03 <sihabunsakibh1@gmail.com>
2025-08-10 02:09:42 +0600

    Stucked

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,22 +3,44 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import s from "../assets/image/s.jpg";
 
-const Cards = () => {
-    return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-     <img src={s} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        </div>
-    );
+
+const Fetch = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((res) => {return res.json();})
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
+  return (
+    <div>
+
+      {products.map((product) => (
+        < />
+      ))}
+    </div>
+  );
 };
 
-export default Cards;
+
+// const Cards = () => {
+//     return (
+//         <div>
+//             <Card style={{ width: '18rem' }}>
+//      <img src={s} />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//         </div>
+//     );
+// };
+
+export default Fetch;
