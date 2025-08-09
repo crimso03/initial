44294fa077baec9551b2b76eca2202a78c7abb49
crimso03 <sihabunsakibh1@gmail.com>
2025-08-10 01:36:51 +0600

    previous file upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './components/Hero';
+import Cards from './components/Cards';
+import Nav from './components/Nav';
+import Tb from './components/Tb';
+
+function App() {
+ 
+  return (
+    <>
+    
+      
+      
+        <Nav/>
+      <Hero/>
+      <Cards/>
+      <Tb/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import Carousel from "react-bootstrap/Carousel";
+import s from "../assets/image/s.jpg";
+
+const componentName = () => {
+  return (
+    <div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img src={s} />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={s} />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={s} />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default componentName;
